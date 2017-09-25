751c06fac5d62d967b197e55cc52f351585c9524
Emily Wells Jimenez <emwellspdx@gmail.com>
2017-09-25 00:09:03 -0700

    add Business and UIlogic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
-<<<<<<< HEAD
-=======
 // Business logic goes here
-
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+}
 
 
 
@@ -9,7 +10,17 @@
 
 // UI logic goes here
 $(document).ready(function() {
-  
+  $("form#new-contact").submit(function(event) {
+      event.preventDefault();
+
+      var inputtedFirstName = $("input#new-first-name").val();
+      var inputtedLastName = $("input#new-last-name").val();
+
+      var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+      $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+
+      $("input#new-first-name").val("");
+      $("input#new-last-name").val("");
   });
 });
->>>>>>> eb67a84... change file structure.
